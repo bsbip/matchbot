aa875fc9101d4873b998e2d1d3cce32b384bc6b4
Sander <sander@bsbip.com>
2021-11-03 17:19:32 +0100

    Upgrade to Angular 11

--- angular/src/app/pages/players/players.component.spec.ts
+++ angular/src/app/pages/players/players.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PlayersComponent } from './players.component';
 
@@ -6,7 +6,7 @@ describe('PlayersComponent', () => {
   let component: PlayersComponent;
   let fixture: ComponentFixture<PlayersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PlayersComponent ]
     })
--- angular/src/app/pages/standings/standings.component.spec.ts
+++ angular/src/app/pages/standings/standings.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StandingsComponent } from './standings.component';
 
@@ -6,7 +6,7 @@ describe('StandingsComponent', () => {
   let component: StandingsComponent;
   let fixture: ComponentFixture<StandingsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StandingsComponent ]
     })
